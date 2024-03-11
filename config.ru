677ad6cfd6bc767ba741915b8dff754134e4aa1f
Thiago Alencar <talencar@gmail.com>
2024-03-11 15:08:05 -0300

    build: start project

---
+++ config.ru
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require './config/application'
+run BestQuotes::Application.new
---
+++ config/application.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'on_rules'
+
+module BestQuotes
+  class Application < OnRules::Application
+  end
+end
